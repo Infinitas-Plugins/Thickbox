93decbaf309168d6ea11d0ff4b179986b834232d
dogmatic69 <dogmatic69@gmail.com>
2012-09-27 18:03:31 +0100

    fixing up the code formatting

--- webroot/js/thickbox.js
+++ webroot/js/thickbox.js
@@ -15,10 +15,11 @@ $(document).ready(function() {
 //add thickbox to href & area elements that have a class of .thickbox
 function tb_init() {
 	$('a.thickbox, area.thickbox, input.thickbox').live('click', function(){
-		var t = this.title || this.name || null;
-		var a = this.href || this.alt;
-		var g = this.rel || false;
-		tb_show(t,a,g);
+		var caption = this.title || this.name || null,
+			url = this.href || this.alt,
+			imageGroup = this.rel || false;
+
+		tb_show(caption, url, imageGroup);
 		this.blur();
 		return false;
 	});
@@ -51,11 +52,9 @@ function tb_show(caption, url, imageGroup) {//function called when the user clic
 		$("body").append("<div id='TB_load'><img src='"+imgLoader.src+"' /></div>");//add loader to the page
 		$('#TB_load').show();//show loader
 
-		var baseURL;
+		var baseURL = url;
 	   if(url.indexOf("?")!==-1){ //ff there is a query string involved
 			baseURL = url.substr(0, url.indexOf("?"));
-	   }else{
-	   		baseURL = url;
 	   }
 
 	   var urlString = /\.jpg$|\.jpeg$|\.png$|\.gif$|\.bmp$/;
@@ -185,24 +184,25 @@ function tb_show(caption, url, imageGroup) {//function called when the user clic
 			ajaxContentW = TB_WIDTH - 30;
 			ajaxContentH = TB_HEIGHT - 45;
 
-			if(url.indexOf('TB_iframe') != -1){// either iframe or ajax window
+			if (url.indexOf('TB_iframe') != -1) {// either iframe or ajax window
 					urlNoQuery = url.split('TB_');
 					$("#TB_iframeContent").remove();
-					if(params['modal'] != "true"){//iframe no modal
-						$("#TB_window").append("<div id='TB_title'><div id='TB_ajaxWindowTitle'>"+caption+"</div><div id='TB_closeAjaxWindow'><a href='#' id='TB_closeWindowButton' title='Close'>close</a> or Esc Key</div></div><iframe frameborder='0' hspace='0' src='"+urlNoQuery[0]+"' id='TB_iframeContent' name='TB_iframeContent"+Math.round(Math.random()*1000)+"' onload='tb_showIframe()' style='width:"+(ajaxContentW + 29)+"px;height:"+(ajaxContentH + 17)+"px;' > </iframe>");
-					}else{//iframe modal
-					$("#TB_overlay").unbind();
+					if(params['modal'] != "true") {//iframe no modal
+						$("#TB_window").append(
+							"<div id='TB_title'><div id='TB_ajaxWindowTitle'>" + caption + "</div><div id='TB_closeAjaxWindow'><a href='#' id='TB_closeWindowButton' title='Close'>close</a> or Esc Key</div></div><iframe frameborder='0' hspace='0' src='"+urlNoQuery[0]+"' id='TB_iframeContent' name='TB_iframeContent"+Math.round(Math.random()*1000)+"' onload='tb_showIframe()' style='width:"+(ajaxContentW + 29)+"px;height:"+(ajaxContentH + 17)+"px;' > </iframe>");
+					} else {//iframe modal
+						$("#TB_overlay").unbind();
 						$("#TB_window").append("<iframe frameborder='0' hspace='0' src='"+urlNoQuery[0]+"' id='TB_iframeContent' name='TB_iframeContent"+Math.round(Math.random()*1000)+"' onload='tb_showIframe()' style='width:"+(ajaxContentW + 29)+"px;height:"+(ajaxContentH + 17)+"px;'> </iframe>");
 					}
-			}else{// not an iframe, ajax
-					if($("#TB_window").css("display") != "block"){
+			} else {// not an iframe, ajax
+					if ($("#TB_window").css("display") != "block") {
 						if(params['modal'] != "true"){//ajax no modal
-						$("#TB_window").append("<div id='TB_title'><div id='TB_ajaxWindowTitle'>"+caption+"</div><div id='TB_closeAjaxWindow'><a href='#' id='TB_closeWindowButton'>close</a> or Esc Key</div></div><div id='TB_ajaxContent' style='width:"+ajaxContentW+"px;height:"+ajaxContentH+"px'></div>");
+						$("#TB_window").append("<div id='TB_title'><div id='TB_ajaxWindowTitle'>" + caption + "</div><div id='TB_closeAjaxWindow'><a href='#' id='TB_closeWindowButton'>close</a> or Esc Key</div></div><div id='TB_ajaxContent' style='width:"+ajaxContentW+"px;height:"+ajaxContentH+"px'></div>");
 						}else{//ajax modal
 						$("#TB_overlay").unbind();
 						$("#TB_window").append("<div id='TB_ajaxContent' class='TB_modal' style='width:"+ajaxContentW+"px;height:"+ajaxContentH+"px;'></div>");
 						}
-					}else{//this means the window is already up, we are just loading new content via ajax
+					} else{//this means the window is already up, we are just loading new content via ajax
 						$("#TB_ajaxContent")[0].style.width = ajaxContentW +"px";
 						$("#TB_ajaxContent")[0].style.height = ajaxContentH +"px";
 						$("#TB_ajaxContent")[0].scrollTop = 0;
@@ -212,28 +212,28 @@ function tb_show(caption, url, imageGroup) {//function called when the user clic
 
 			$("#TB_closeWindowButton").click(tb_remove);
 
-				if(url.indexOf('TB_inline') != -1){
-					$("#TB_ajaxContent").append($('#' + params['inlineId']).children());
-					$("#TB_window").unload(function () {
-						$('#' + params['inlineId']).append( $("#TB_ajaxContent").children() ); // move elements back when you're finished
-					});
-					tb_position();
+			if (url.indexOf('TB_inline') != -1) {
+				$("#TB_ajaxContent").append($('#' + params['inlineId']).children());
+				$("#TB_window").unload(function () {
+					$('#' + params['inlineId']).append( $("#TB_ajaxContent").children() ); // move elements back when you're finished
+				});
+				tb_position();
+				$("#TB_load").remove();
+				$("#TB_window").css({display:"block"});
+			} else if(url.indexOf('TB_iframe') != -1) {
+				tb_position();
+				if($.browser.safari){//safari needs help because it will not fire iframe onload
 					$("#TB_load").remove();
 					$("#TB_window").css({display:"block"});
-				}else if(url.indexOf('TB_iframe') != -1){
-					tb_position();
-					if($.browser.safari){//safari needs help because it will not fire iframe onload
-						$("#TB_load").remove();
-						$("#TB_window").css({display:"block"});
-					}
-				}else{
-					$("#TB_ajaxContent").load(url += "&random=" + (new Date().getTime()),function(){//to do a post change this load method
-						tb_position();
-						$("#TB_load").remove();
-						tb_init("#TB_ajaxContent a.thickbox");
-						$("#TB_window").css({display:"block"});
-					});
 				}
+			} else {
+				$("#TB_ajaxContent").load(url += "&random=" + (new Date().getTime()),function(){//to do a post change this load method
+					tb_position();
+					$("#TB_load").remove();
+					tb_init("#TB_ajaxContent a.thickbox");
+					$("#TB_window").css({display:"block"});
+				});
+			}
 
 		}
 
