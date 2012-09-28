1a7adc9db28dee534bb0a9f1c132349687dbe3e8
dogmatic69 <dogmatic69@gmail.com>
2012-09-29 00:45:55 +0100

    adding tests

---
+++ Test/Fixture/Case/Lib/ThickboxEventsTest.php
@@ -0,0 +1,15 @@
+<?php
+App::uses('ThickboxEvents', 'Thickbox.Lib');
+class ThickboxEventsTest extends CakeTestCase {
+	public function setUp() {
+		parent::setUp();
+	}
+
+	public function tearDown() {
+		parent::tearDown();
+	}
+
+	public function testSomething() {
+
+	}
+}
