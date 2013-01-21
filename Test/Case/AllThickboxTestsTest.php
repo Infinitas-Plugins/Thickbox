349a381b6785da6231d36a28db85a199283b9aeb
dogmatic69 <dogmatic69@gmail.com>
2013-01-21 06:54:14 +0000

    adding coverage filter

--- Test/Case/AllThickboxTestsTest.php
+++ Test/Case/AllThickboxTestsTest.php
@@ -1,5 +1,7 @@
 <?php
-class AllThickboxTestsTest extends PHPUnit_Framework_TestSuite {
+App::uses('AllTestsBase', 'Test/Lib');
+
+class AllThickboxTestsTest extends AllTestsBase {
 
 /**
  * Suite define the tests for this suite
